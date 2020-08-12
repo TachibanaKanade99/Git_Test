9fa0eee10db8086e8a29c04182d0ac9f4377ad79
TachibanaKanade99 <sad361975@gmail.com>
2020-08-12 11:25:16 +0700

    add Bootstrap Exercise

---
+++ Exercise/Bootstrap/src.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // $("#modal_1").bind("click", function(){
+    //     $("#exampleModal1").modal("show");
+    // });
+    $("#modal_2").bind("click", function () {
+        $("#exampleModal2").modal("show");
+    });
+});
