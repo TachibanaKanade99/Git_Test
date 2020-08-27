7773cb4934bb263072ac59a13e4c881b25340772
Tuan Minh <sad361975@gmail.com>
2020-08-27 13:39:13 +0700

    modify routes

--- Final-App/config/routes.rb
+++ Final-App/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+
+  # Resourceful
+
   # Admin
   namespace :admin do
-  	resources :users, :photos, :albums
+  	resources :users, :photos, :albums, only: [:show, :edit, :update, :destroy]
   end
 
   # User
@@ -12,6 +15,17 @@ Rails.application.routes.draw do
     resources :photos
     resources :likes
   end
+
+  # Albums
+  namespace :albums do
+    resources :photos, only: [:edit, :update, :show, :destory]
+  end
+
+  # Non-resourceful
+  root 'home/index'
+  get '/feed', to: 'home#feed'
+  get 'exit', to: 'session#destroy', as: :logout
+
 
 
 end
