f2ac5d577532d80a1f4f5af40fa8b1d17e60c541
Tuan Minh <sad361975@gmail.com>
2020-09-01 15:57:23 +0700

    Update Integrate  Bootstrap and JQuery Exercise

--- Final-App/app/controllers/album_controller.rb
+++ Final-App/app/controllers/album_controller.rb
@@ -1,6 +1,11 @@
 class AlbumController < ApplicationController
-	def edit
+	before_action :getAlbum, only: [:edit, :update]
+
+	def getAlbum
 		@album = Album.find(params[:id])
+	end
+
+	def edit
 	end
 
 	def show
@@ -8,19 +13,19 @@ class AlbumController < ApplicationController
 	
 	def update
 		@new_title = params[:album][:title]
-		@album = Album.find(params[:id])
 		@album.title = @new_title
 		
 		if @album.update(album_params)
-			# @album.update!(album_params)
+			# flash[:notice] = 'Update Successfully'
+			# redirect_to edit_album_path
 			redirect_to newest_path
 		else 
 			flash[:error] = 'Title is too short'
-			redirect_to edit_album_path
+			flash.now[:error] = 'Title is too short'
+			render "edit"
+			# redirect_to edit_album_path
 		end
 	end
-
-
 
 	private 
 		def album_params
